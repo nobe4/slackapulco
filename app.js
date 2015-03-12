9c69ed291c7840772398c1f24a39af751e7582df
nobe4 <haffreinguev@gmail.com>
2015-03-12 20:24:23 +0100

    added verify token

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var app = express();
+var parser = require('body-parser').urlencoded({ extended: false  });
 var port = process.env.PORT || 3000;
+var token = process.env.TOKEN || 'secretToken';
 
 app.get('/', function (req, res) {
-	res.send('Hello World!');
+	res.send('Nothing here :O');
 });
 
-var server = app.listen(3000, function () {
-	console.log('Magic on port %s',   server.address().port);
+app.post('/', parser , function (req, res) {
+	if(req.body.token === token){
+		console.log(req.body);
+		res.send('ok');
+	} else {
+		res.status(404).send('Bad token');
+	}
+});
+
+app.listen(port, function () {
+	console.log('Magic on port %s',  port);
 });
